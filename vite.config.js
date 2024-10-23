6699381574b418318e5fdc156fd1874ea56f3600
marsheltatiana <marsheltatianasanchezvigo@gmail.com>
2024-10-23 00:12:37 -0500

    Se añadio el transpilado de commonjs a ESM

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { resolve } from 'path'
+import { resolve } from "path";
+import commonjs from "vite-plugin-commonjs";
 
 export default {
-  root: resolve(__dirname, 'src'),
+  root: resolve(__dirname, "src"),
   build: {
-    outDir: '../dist-olimpus'
+    outDir: "../dist-olimpus",
   },
   server: {
-    port: 8080
-  }
-}
+    port: 8080,
+  },
+  plugins: [commonjs()],
+};
